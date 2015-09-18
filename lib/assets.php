705ee0e2ca6a3eee4fcbb9cbef6bbd0fe21844ed
Karan Nijhawan <karannijhawan@gizmee.com>
2015-09-18 18:29:21 +0200

    as

--- lib/assets.php
+++ lib/assets.php
@@ -72,7 +72,7 @@ function assets() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
-  wp_enqueue_script('sage/js', asset_path('scripts/bundle.js'), [], null, true);
+  wp_enqueue_script('modernizr', asset_path('/modernizr.js'), [], null, true);
+  wp_enqueue_script('sage/js', asset_path('/bundle.js'), [], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
