f26d9ea6d1ca310ee89c9f641c0f0cd6e493d645
Karan Nijhawan <karannijhawan@gizmee.com>
2015-09-20 13:39:32 +0200

    remove scss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import gutil from 'gulp-util';
 import webpack from 'webpack';
 import postcss from 'gulp-postcss';
+import atImport from 'postcss-import';
 import precss from 'precss';
 import autoprefixer from 'autoprefixer';
 import minmax from 'postcss-media-minmax';
@@ -11,6 +12,8 @@ import sourcemaps from 'gulp-sourcemaps';
 import browserSync from 'browser-sync';
 import dotenv from 'dotenv';
 import path from 'path';
+import plumber from 'gulp-plumber'
+import postCssSimpleVariables from 'postcss-simple-vars';
 
 dotenv.config({path: path.resolve(__dirname, '../../../../.env')})
 dotenv.load();
@@ -24,15 +27,23 @@ const paths = {
 };
 
 
+const onError = function (err) {
+  gutil.beep();
+  console.log(err);
+  this.emit('end');
+};
+
 gulp.task('css', function () {
     var processors = [
       cssnano({autoprefixer: false, sourcemap: true}),
       autoprefixer({browsers: ['> 1%','IE 9','IE 10','Firefox >= 10']}),
-      precss,
+      postCssSimpleVariables,
       minmax,
-      postcssNeat({neatMaxWidth: '80em'})
+      postcssNeat({neatMaxWidth: '80em'}),
+      atImport
     ];
     return gulp.src('assets/styles/main.css')
+      .pipe(plumber({errorHandler: onError}))
       .pipe(sourcemaps.init())
       .pipe(postcss(processors))
       .pipe(sourcemaps.write('.'))
